56f10c0ffb5bd6ef815c4f24b7b91197dc1e1774
sergeynagorny <sergey.s.nagorny@gmail.com>
2020-11-13 18:07:09 +0300

    Улучшение окна настройки
    - При клике на ESC окнко не закроется если инпут в фокусе

--- js/setup.js
+++ js/setup.js
@@ -75,7 +75,7 @@
   };
 
   var onSetupEscPress = function (evt) {
-    if (evt.keyCode === KEYCODE.ESC) {
+    if (evt.keyCode === KEYCODE.ESC && evt.target.tagName.toLowerCase() !== 'input') {
       setupClose();
     }
   };
@@ -125,30 +125,23 @@
   var wizardCoatInput = document.querySelector('input[name=coat-color]');
   var wizardEyesInput = document.querySelector('input[name=eyes-color]');
   var wizardFireballInput = document.querySelector('input[name=fireball-color]');
-  console.log(wizardCoatInput);
-  console.log(wizardEyesInput);
-  console.log(wizardFireballInput);
-
 
   wizardCoat.addEventListener('click', function () {
     var randomColor = window.wizard.getRandomValue(window.wizard.colorCoat);
     wizardCoat.style.fill = randomColor;
     wizardCoatInput.value = randomColor;
-    console.log(wizardCoatInput.value);
   });
 
   wizardEyes.addEventListener('click', function () {
     var randomColor = window.wizard.getRandomValue(window.wizard.colorEyes);
     wizardEyes.style.fill = randomColor;
     wizardEyesInput.value = randomColor;
-    console.log(wizardEyesInput.value);
   });
 
   wizardFireball.addEventListener('click', function () {
     var randomColor = window.wizard.getRandomValue(window.wizard.colorFireball);
     wizardFireball.style.background = randomColor;
     wizardFireballInput.value = randomColor;
-    console.log(wizardFireballInput.value);
   });
 
 })();
