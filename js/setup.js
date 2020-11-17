174803fda64bc3a93809fe656466f9747e459c21
sergeynagorny <sergey.s.nagorny@gmail.com>
2020-11-17 15:36:05 +0300

    Оптимизировал функцию кастомизации волшебника

--- js/setup.js
+++ js/setup.js
@@ -2,33 +2,44 @@
 
 (function () {
 
-  // Кастомизация волшебника
-  // FIXME: Переписать с замыканиями, как положенно
+  var wizardSetup = [
+    {
+      element: document.querySelector('.wizard-coat'),
+      data: window.wizard.colorCoat,
+      method: 'fill',
+      input: document.querySelector('input[name=coat-color]'),
+    },
+    {
+      element: document.querySelector('.wizard-eyes'),
+      data: window.wizard.colorEyes,
+      method: 'fill',
+      input: document.querySelector('input[name=eyes-color]'),
+    },
+    {
+      element: document.querySelector('.setup-fireball-wrap'),
+      data: window.wizard.colorFireball,
+      method: 'background',
+      input: document.querySelector('input[name=fireball-color]'),
+    },
+  ];
 
-  var setupWizard = document.querySelector('.setup-player');
-  var wizardCoat = setupWizard.querySelector('.wizard-coat');
-  var wizardEyes = setupWizard.querySelector('.wizard-eyes');
-  var wizardFireball = setupWizard.querySelector('.setup-fireball-wrap');
-  var wizardCoatInput = document.querySelector('input[name=coat-color]');
-  var wizardEyesInput = document.querySelector('input[name=eyes-color]');
-  var wizardFireballInput = document.querySelector('input[name=fireball-color]');
+  var addClickListener = function (item) {
+    var i = 0;
+    item.element.addEventListener('click', function () {
+      i = i >= item.data.length - 1 ? 0 : ++i;
 
-  wizardCoat.addEventListener('click', function () {
-    var randomColor = window.wizard.getRandomValue(window.wizard.colorCoat);
-    wizardCoat.style.fill = randomColor;
-    wizardCoatInput.value = randomColor;
-  });
+      if (item.method === 'background') {
+        item.element.style.background = item.data[i];
+      } else {
+        item.element.style.fill = item.data[i];
+      }
 
-  wizardEyes.addEventListener('click', function () {
-    var randomColor = window.wizard.getRandomValue(window.wizard.colorEyes);
-    wizardEyes.style.fill = randomColor;
-    wizardEyesInput.value = randomColor;
-  });
+      item.input.value = item.data[i];
+    });
+  };
 
-  wizardFireball.addEventListener('click', function () {
-    var randomColor = window.wizard.getRandomValue(window.wizard.colorFireball);
-    wizardFireball.style.background = randomColor;
-    wizardFireballInput.value = randomColor;
+  wizardSetup.forEach(function (wizardItem) {
+    addClickListener(wizardItem);
   });
 
 })();
