55ff4460ccfcd77ac16e932fb9b7e360da8890a7
sergeynagorny <sergey.s.nagorny@gmail.com>
2020-11-17 13:28:56 +0300

    Добавил функцию перетягивания окна настроек

---
+++ js/dialog.js
@@ -0,0 +1,123 @@
+'use strict';
+
+(function () {
+
+  // TODO:
+  // Если диалог открыт, нажатие на кнопку сохранить приводит к отправке формы
+  // Если диалог открыт и фокус находится на "Сохранить" нажатие на ENTER приводит к отправке формы
+
+  var dialog = document.querySelector('.setup');
+  var dialogBtnOpen = document.querySelector('.setup-open');
+  var dialogBtnClose = document.querySelector('.setup-close');
+
+  var KEYCODE = {
+    ENTER: 13,
+    ESC: 27,
+  };
+
+  var dialogDefaultCoords = {
+    x: window.getComputedStyle(dialog).left,
+    y: window.getComputedStyle(dialog).top,
+  };
+
+  var setDefaultCoords = function () {
+    dialog.style.left = dialogDefaultCoords.x;
+    dialog.style.top = dialogDefaultCoords.y;
+  };
+
+  var onDialogEscPress = function (evt) {
+    if (evt.keyCode === KEYCODE.ESC && evt.target.tagName.toLowerCase() !== 'input') {
+      setupClose();
+    }
+  };
+
+  var dialogOpen = function () {
+    setDefaultCoords();
+    dialog.classList.remove('hidden');
+    document.addEventListener('keydown', onDialogEscPress);
+  };
+
+  var dialogClose = function () {
+    dialog.classList.add('hidden');
+    document.removeEventListener('keydown', onDialogEscPress);
+  };
+
+  dialogBtnOpen.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === KEYCODE.ENTER) {
+      if (dialog.classList.contains('hidden')) {
+        dialogOpen();
+      } else {
+        dialogClose();
+      }
+    }
+  });
+
+  dialogBtnClose.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === KEYCODE.ENTER) {
+      dialogClose();
+    }
+  });
+
+  dialogBtnOpen.addEventListener('click', function () {
+    dialogOpen();
+  });
+
+  dialogBtnClose.addEventListener('click', function () {
+    dialogClose();
+  });
+
+})();
+
+(function () {
+
+  var dialog = document.querySelector('.setup');
+  var dialogHandle = document.querySelector('.upload');
+
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY,
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY,
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY,
+      };
+
+      dialog.style.top = (dialog.offsetTop - shift.y) + 'px';
+      dialog.style.left = (dialog.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onCLickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          dialogHandle.removeEventListener('click', onCLickPreventDefault);
+        };
+        dialogHandle.addEventListener('click', onCLickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+})();
