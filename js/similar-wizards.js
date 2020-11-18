b620b01abbd2cfd3b941aa87e517c756badd3972
sergeynagorny <sergey.s.nagorny@gmail.com>
2020-11-18 10:21:27 +0300

    Рефакторинг отрисовки похожих волшебников

--- js/similar-wizards.js
+++ js/similar-wizards.js
@@ -1,40 +1,44 @@
 'use strict';
 
 (function () {
-
-  var dataWizards = window.wizard.generateRandomWizards(4);
+  var SIMILAR_WIZARDS_COUNT = 4;
+  var dataWizards = [];
   var setup = document.querySelector('.setup');
 
+  var onLoad = function (data) {
+    dataWizards = data;
+    renderSimilarWizards(data);
+  };
 
-  var createSimilarWizards = function (randomWizards) {
+  window.backend.load(onLoad, window.backend.errorHandler);
+
+  var createSimilarWizards = function (wizardItem) {
     var similarWizardTempalte = document.querySelector('#similar-wizard-template').content;
     var similarWizardItem = similarWizardTempalte.querySelector('.setup-similar-item');
-    var fragment = document.createDocumentFragment();
 
-    for (var i = 0; i < randomWizards.length; i++) {
-      var wizard = similarWizardItem.cloneNode(true);
-      var wizardName = wizard.querySelector('.setup-similar-label');
-      var wizardCoat = wizard.querySelector('.wizard-coat');
-      var wizardEyes = wizard.querySelector('.wizard-eyes');
+    var wizard = similarWizardItem.cloneNode(true);
+    var wizardName = wizard.querySelector('.setup-similar-label');
+    var wizardCoat = wizard.querySelector('.wizard-coat');
+    var wizardEyes = wizard.querySelector('.wizard-eyes');
 
-      wizardName.textContent = randomWizards[i].name;
-      wizardCoat.style.fill = randomWizards[i].colorCoat;
-      wizardEyes.style.fill = randomWizards[i].colorEyes;
+    wizardName.textContent = wizardItem.name;
+    wizardCoat.style.fill = wizardItem.colorCoat;
+    wizardEyes.style.fill = wizardItem.colorEyes;
 
-      fragment.appendChild(wizard);
-    }
+    return wizard;
 
-    return fragment;
   };
 
   var renderSimilarWizards = function (data) {
     var similarWizards = setup.querySelector('.setup-similar');
     var similarWizardsList = similarWizards.querySelector('.setup-similar-list');
-
+    var fragment = document.createDocumentFragment();
     similarWizards.classList.remove('hidden');
-    similarWizardsList.appendChild(createSimilarWizards(data));
-  };
 
-  renderSimilarWizards(dataWizards);
+    for (var i = 0; i < SIMILAR_WIZARDS_COUNT; i++) {
+      fragment.appendChild(createSimilarWizards(data[i]));
+    }
+    similarWizardsList.appendChild(fragment);
+  };
 
 })();
